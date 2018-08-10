4a210cf8c9c02601539537d0d871b0c6404725db
Martin Svendsen <martsve@gmail.com>
2018-08-10 12:28:22 +0200

    Scoring tests

--- CamelCup.Test/RulesEngine/TestScoreGame.cs
+++ CamelCup.Test/RulesEngine/TestScoreGame.cs
@@ -12,7 +12,7 @@ namespace CamelCup.Test
     public class TestScoreGame
     {
         [TestMethod]
-        public void RulesEngine_ScoreGame()
+        public void RulesEngine_ScoreGame_NoBets()
         {
             // 1: blue | 2: green, orange | 3: red, yellow
             var startingPositions = TestHelper.ConvertToStartingPositions("1,0 2,1 2,2 3,3 3,4");
@@ -20,6 +20,92 @@ namespace CamelCup.Test
 
             var engine = new RulesEngine(gamestate, seed: 1);
             engine.ScoreGame();
+
+            Assert.AreEqual(3, gamestate.Money[0], "money p0");
+            Assert.AreEqual(3, gamestate.Money[1], "money p1");
+            Assert.AreEqual(3, gamestate.Money[2], "money p2");
+            Assert.AreEqual(3, gamestate.Money[3], "money p3");
+        }
+
+        [TestMethod]
+        public void RulesEngine_ScoreGame_Winners()
+        {
+            // 1: blue | 2: green, orange | 3: red, yellow
+            var startingPositions = TestHelper.ConvertToStartingPositions("1,0 2,1 2,2 3,3 3,4");
+            var gamestate = new ImplementedGameState(8, startingPositions);
+
+            gamestate.WinnerBets.Add(new GameEndBet() { Player = 7, CamelColor = CamelColor.Yellow }); // 8
+            gamestate.WinnerBets.Add(new GameEndBet() { Player = 5, CamelColor = CamelColor.Yellow }); // 5
+            gamestate.WinnerBets.Add(new GameEndBet() { Player = 3, CamelColor = CamelColor.Yellow }); // 3
+            gamestate.WinnerBets.Add(new GameEndBet() { Player = 2, CamelColor = CamelColor.Yellow }); // 2
+            gamestate.WinnerBets.Add(new GameEndBet() { Player = 1, CamelColor = CamelColor.Yellow });
+            gamestate.WinnerBets.Add(new GameEndBet() { Player = 0, CamelColor = CamelColor.Yellow });
+            gamestate.WinnerBets.Add(new GameEndBet() { Player = 6, CamelColor = CamelColor.Yellow });
+
+            var engine = new RulesEngine(gamestate, seed: 1);
+            engine.ScoreGame(); // 8, 5, 3, 2, 1, 1, 1, 1, 1, 1, 1
+
+            Assert.AreEqual(4, gamestate.Money[0], "money p0");
+            Assert.AreEqual(4, gamestate.Money[1], "money p1");
+            Assert.AreEqual(5, gamestate.Money[2], "money p2");
+            Assert.AreEqual(6, gamestate.Money[3], "money p3");
+            Assert.AreEqual(3, gamestate.Money[4], "money p4");
+            Assert.AreEqual(8, gamestate.Money[5], "money p5");
+            Assert.AreEqual(4, gamestate.Money[6], "money p6");
+            Assert.AreEqual(11, gamestate.Money[7], "money p7");
+        }
+        
+        [TestMethod]
+        public void RulesEngine_ScoreGame_Losers()
+        {
+            // 1: blue | 2: green, orange | 3: red, yellow
+            var startingPositions = TestHelper.ConvertToStartingPositions("1,0 2,1 2,2 3,3 3,4");
+            var gamestate = new ImplementedGameState(8, startingPositions);
+
+            gamestate.LoserBets.Add(new GameEndBet() { Player = 7, CamelColor = CamelColor.Blue }); // 8
+            gamestate.LoserBets.Add(new GameEndBet() { Player = 5, CamelColor = CamelColor.Blue }); // 5
+            gamestate.LoserBets.Add(new GameEndBet() { Player = 3, CamelColor = CamelColor.Blue }); // 3
+            gamestate.LoserBets.Add(new GameEndBet() { Player = 2, CamelColor = CamelColor.Blue }); // 2
+            gamestate.LoserBets.Add(new GameEndBet() { Player = 1, CamelColor = CamelColor.Blue });
+            gamestate.LoserBets.Add(new GameEndBet() { Player = 0, CamelColor = CamelColor.Blue });
+            gamestate.LoserBets.Add(new GameEndBet() { Player = 6, CamelColor = CamelColor.Blue });
+
+            var engine = new RulesEngine(gamestate, seed: 1);
+            engine.ScoreGame(); // 8, 5, 3, 2, 1, 1, 1, 1, 1, 1, 1
+
+            Assert.AreEqual(4, gamestate.Money[0], "money p0");
+            Assert.AreEqual(4, gamestate.Money[1], "money p1");
+            Assert.AreEqual(5, gamestate.Money[2], "money p2");
+            Assert.AreEqual(6, gamestate.Money[3], "money p3");
+            Assert.AreEqual(3, gamestate.Money[4], "money p4");
+            Assert.AreEqual(8, gamestate.Money[5], "money p5");
+            Assert.AreEqual(4, gamestate.Money[6], "money p6");
+            Assert.AreEqual(11, gamestate.Money[7], "money p7");
+        }
+        
+        [TestMethod]
+        public void RulesEngine_ScoreGame_WrongBets()
+        {
+            // 1: blue | 2: green, orange | 3: red, yellow
+            var startingPositions = TestHelper.ConvertToStartingPositions("1,0 2,1 2,2 3,3 3,4");
+            var gamestate = new ImplementedGameState(4, startingPositions);
+
+            gamestate.LoserBets.Add(new GameEndBet() { Player = 0, CamelColor = CamelColor.Green });
+            gamestate.LoserBets.Add(new GameEndBet() { Player = 0, CamelColor = CamelColor.Orange });
+            gamestate.LoserBets.Add(new GameEndBet() { Player = 0, CamelColor = CamelColor.Red });
+
+            gamestate.LoserBets.Add(new GameEndBet() { Player = 1, CamelColor = CamelColor.Orange });
+            gamestate.LoserBets.Add(new GameEndBet() { Player = 1, CamelColor = CamelColor.Red });
+
+            gamestate.LoserBets.Add(new GameEndBet() { Player = 2, CamelColor = CamelColor.Red });
+
+            var engine = new RulesEngine(gamestate, seed: 1);
+            engine.ScoreGame();
+
+            Assert.AreEqual(0, gamestate.Money[0], "money p0");
+            Assert.AreEqual(1, gamestate.Money[1], "money p1");
+            Assert.AreEqual(2, gamestate.Money[2], "money p2");
+            Assert.AreEqual(3, gamestate.Money[3], "money p3");
         }
     }
 }
--- CamelCup.Test/RulesEngine/TestScoreRound.cs
+++ CamelCup.Test/RulesEngine/TestScoreRound.cs
@@ -12,7 +12,7 @@ namespace CamelCup.Test
     public class TestScoreRound
     {
         [TestMethod]
-        public void RulesEngine_ScoreRound()
+        public void RulesEngine_ScoreRound_NoBets()
         {
             // 1: blue | 2: green, orange | 3: red, yellow
             var startingPositions = TestHelper.ConvertToStartingPositions("1,0 2,1 2,2 3,3 3,4");
@@ -20,6 +20,75 @@ namespace CamelCup.Test
 
             var engine = new RulesEngine(gamestate, seed: 1);
             engine.ScoreRound();
+            
+            Assert.AreEqual(3, gamestate.Money[0], "money p0");
+            Assert.AreEqual(3, gamestate.Money[1], "money p1");
+            Assert.AreEqual(3, gamestate.Money[2], "money p2");
+            Assert.AreEqual(3, gamestate.Money[3], "money p3");
         }
+
+       [TestMethod]
+        public void RulesEngine_ScoreRound_Bets_Winner()
+        {
+            // 1: blue | 2: green, orange | 3: red, yellow
+            var startingPositions = TestHelper.ConvertToStartingPositions("1,0 2,1 2,2 3,3 3,4");
+            var gamestate = new ImplementedGameState(4, startingPositions);
+
+            gamestate.BettingCards.First(x => x.CamelColor == CamelColor.Yellow && x.Value == 5).Owner = 0;
+            gamestate.BettingCards.First(x => x.CamelColor == CamelColor.Yellow && x.Value == 3).Owner = 1;
+            gamestate.BettingCards.First(x => x.CamelColor == CamelColor.Yellow && x.Value == 2).Owner = 2;
+
+            var engine = new RulesEngine(gamestate, seed: 1);
+            engine.ScoreRound();
+            
+            Assert.AreEqual(8, gamestate.Money[0], "money p0");
+            Assert.AreEqual(6, gamestate.Money[1], "money p1");
+            Assert.AreEqual(5, gamestate.Money[2], "money p2");
+            Assert.AreEqual(3, gamestate.Money[3], "money p3");
+        }     
+        
+       [TestMethod]
+        public void RulesEngine_ScoreRound_Bets_SecondPlace()
+        {
+            // 1: blue | 2: green, orange | 3: red, yellow
+            var startingPositions = TestHelper.ConvertToStartingPositions("1,0 2,1 2,2 3,3 3,4");
+            var gamestate = new ImplementedGameState(4, startingPositions);
+
+            gamestate.BettingCards.First(x => x.CamelColor == CamelColor.Red && x.Value == 5).Owner = 0;
+            gamestate.BettingCards.First(x => x.CamelColor == CamelColor.Red && x.Value == 3).Owner = 1;
+            gamestate.BettingCards.First(x => x.CamelColor == CamelColor.Red && x.Value == 2).Owner = 2;
+
+            var engine = new RulesEngine(gamestate, seed: 1);
+            engine.ScoreRound();
+            
+            Assert.AreEqual(4, gamestate.Money[0], "money p0");
+            Assert.AreEqual(4, gamestate.Money[1], "money p1");
+            Assert.AreEqual(4, gamestate.Money[2], "money p2");
+            Assert.AreEqual(3, gamestate.Money[3], "money p3");
+        }     
+        
+       [TestMethod]
+        public void RulesEngine_ScoreRound_Bets_Loser()
+        {
+            // 1: blue | 2: green, orange | 3: red, yellow
+            var startingPositions = TestHelper.ConvertToStartingPositions("1,0 2,1 2,2 3,3 3,4");
+            var gamestate = new ImplementedGameState(4, startingPositions);
+
+            gamestate.BettingCards.First(x => x.CamelColor == CamelColor.Orange && x.Value == 5).Owner = 0;
+            gamestate.BettingCards.First(x => x.CamelColor == CamelColor.Green && x.Value == 5).Owner = 0;
+
+            gamestate.BettingCards.First(x => x.CamelColor == CamelColor.Orange && x.Value == 3).Owner = 1;
+            gamestate.BettingCards.First(x => x.CamelColor == CamelColor.Blue && x.Value == 5).Owner = 1;
+
+            gamestate.BettingCards.First(x => x.CamelColor == CamelColor.Orange && x.Value == 2).Owner = 2;
+
+            var engine = new RulesEngine(gamestate, seed: 1);
+            engine.ScoreRound();
+            
+            Assert.AreEqual(1, gamestate.Money[0], "money p0");
+            Assert.AreEqual(1, gamestate.Money[1], "money p1");
+            Assert.AreEqual(2, gamestate.Money[2], "money p2");
+            Assert.AreEqual(3, gamestate.Money[3], "money p3");
+        }     
     }
 }
